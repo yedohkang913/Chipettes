cb3e6d9b9c8e68bb9db020449bd4da08be8fa75d
v-law <vlawren@stuy.edu>
2017-03-31 08:39:40 -0400

    Added DDeque.java
    class that implements Deque interface

---
+++ DDeque.java
@@ -0,0 +1,24 @@
+public class DDeque<T> implements Deque<T>{
+
+    private int _size;
+    
+    public void addFirst(T deqVal){
+    }
+    public void addLast(T deqVal){
+    }
+    
+    public T removeFirst(){
+    }
+    public T removeLast(){
+    }
+    
+    public int size(){
+	return _size;
+    }
+    
+    public T getFirst(){
+    }
+    public T getLast(){
+    }
+    
+}
