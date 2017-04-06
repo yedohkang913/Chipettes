90f3c3f5f4a565166a7c7fbc25a51ea239b98a62
chan lin <klin11@stuy.edu>
2017-04-05 22:45:30 -0400

    added comments to interface

--- Deque.java
+++ Deque.java
@@ -1,19 +1,26 @@
+
 public interface Deque<T>{
 
+    //add elements to front and end of deque
     public void addFirst(T deqVal);
     public void addLast(T deqVal);
     
+    //removes elements from front and end of deque and returns removed item; exception if empty
     public T removeFirst();
     public T removeLast();
     
+    //checks if given item is present in deque and returns boolean
     public boolean contains (T deqVal);
     
+    //searches for item in deque and removes the first one or the last one
     public boolean removeFirstOccurrence (T deqVal);
     public boolean removeLastOccurrence (T deqVal);
         
+    //retrieves item in first or last position in deque
     public T getFirst();
     public T getLast();
     
+    //retrieves value stored in size variable
     public int size();
     
 }
